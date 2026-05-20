1a0d520b02822a0f558af09828ac731d8f0980c2
lusama02 <164710899+lusama02@users.noreply.github.com>
2026-05-20 16:58:35 -0300

    cria lista de nomes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,27 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace Lista;
 
 public partial class MainWindow : Window
 {
+    public ObservableCollection<string> nomes { get; set; } = new();
+    
     public MainWindow()
     {
         InitializeComponent();
+
+        this.DataContext = this;
+    }
+
+    private void BtnAdicionaNome_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(tbNome.Text) || tbNome.Text.Length < 3)
+        {
+            MessageBox.Show("Escreva um nome válido!");
+            return;
+        }
+        
+        nomes.Add(tbNome.Text);
     }
 }
