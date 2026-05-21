8a2903c77f7863a3ec8a045371eb9f1beb176e11
lusama02 <164710899+lusama02@users.noreply.github.com>
2026-05-21 16:08:38 -0300

    adiciona funções de remover e encontrar nomes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace Lista;
 public partial class MainWindow : Window
 {
     public ObservableCollection<string> nomes { get; set; } = new();
-    
+
     public MainWindow()
     {
         InitializeComponent();
@@ -21,7 +21,43 @@ public partial class MainWindow : Window
             MessageBox.Show("Escreva um nome válido!");
             return;
         }
-        
+
         nomes.Add(tbNome.Text);
+    }
+
+    private void BtnRemoveNome_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (!nomes.Contains(tbNome.Text, StringComparer.CurrentCultureIgnoreCase))
+        {
+            MessageBox.Show("Nome não encontrado!");
+            return;
+        }
+
+        var nomeEncontrado = nomes.FirstOrDefault(nomePessoa =>
+            nomePessoa.Equals(tbNome.Text, StringComparison.CurrentCultureIgnoreCase));
+
+        nomes.Remove(nomeEncontrado);
+        MessageBox.Show("Nome removido com sucesso!");
+    }
+
+    private void BtnEncontrarNomes_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(tbNome.Text))
+        {
+            MessageBox.Show("Escreva um nome válido!");
+            return;
+        }
+
+        lbNomes.SelectedItems.Clear();
+
+        string termoBuscaMinusculo = tbNome.Text.ToLower();
+
+        foreach (var nome in nomes)
+        {
+            if (nome.Contains(termoBuscaMinusculo, StringComparison.CurrentCultureIgnoreCase))
+            {
+                lbNomes.SelectedItems.Add(nome);
+            }
+        }
     }
 }
